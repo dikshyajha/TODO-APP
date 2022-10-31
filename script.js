8ed665aa28682b1ba10d5cddce20905fc2855262
Dikshya Jha <dikshya.k.jha@gmail.com>
2022-10-31 20:25:58 +0545

    feat: add new features to update the list item

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ form.addEventListener("submit", (event) => {
    formInput.value=""; //makes box empty after adding value
     }
 });
+
+//update part
+const lists = document.querySelectorAll("#tasklist ul");
+lists.forEach(element => {
+    element.addEventListener("click", (event) => {
+        if(event.target.className === "update"){
+            const li = event.target.parentElement;
+            const p = li.querySelector("p");
+            const input = document.createElement("input");
+            input.type="text";
+            input.value=p.innerText;
+            li.insertBefore(input, p);
+            li.removeChild(p);
+            event.target.innerText="save";
+            event.target.className="save";
+        }
+        else if(event.target.className === "save"){
+            const li = event.target.parentElement;
+            const input = li.querySelector("input[type='text']");
+            const p = document.createElement("p");
+            p.innerText=input.value;
+            p.className="coursetitle";
+            li.insertBefore(p, input);
+            li.removeChild(input);
+            event.target.innerText="update";
+            event.target.className="update";
+        }
+    })
+})
